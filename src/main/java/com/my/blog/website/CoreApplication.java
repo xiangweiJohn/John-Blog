e9aef9daebe55ee136f9f4562563f5da1d25a02a
vincent <vincentzxw0502@163.com>
2018-04-11 00:33:31 +0800

    让服务器同事支持http，https

--- src/main/java/com/my/blog/website/CoreApplication.java
+++ src/main/java/com/my/blog/website/CoreApplication.java
@@ -12,7 +12,6 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
 
 @MapperScan("com.my.blog.website.dao")
@@ -38,7 +37,52 @@ public class CoreApplication {
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
+    
+    /*
+     * 把此EmbeddedServletContainerFactory 注入到web容器中
+     */
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+        	//拦截所有的/*请求
+            @Override
+            protected void postProcessContext(Context context) {
+                //Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS. 
+                //You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
 
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+        
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+
+        //Set the scheme that will be assigned to requests received through this connector
+        //@param scheme The new scheme
+        connector.setScheme("http");
+
+        //Set the port number on which we listen for requests.
+        // @param port The new port number
+        connector.setPort(80);       
+
+        //Set the secure connection flag that will be assigned to requests received through this connector.
+        //@param secure The new secure connection flag
+        //if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
+        connector.setSecure(false);
+
+        //redirectPort The redirect port number (non-SSL to SSL)
+        connector.setRedirectPort(443);
+        return connector;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
