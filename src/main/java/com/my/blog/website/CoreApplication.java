44f58efcd4b51606f97233447c5311c26c22d200
vincent <vincentzxw0502@163.com>
2018-04-12 17:46:09 +0800

    nginx configure ssl

--- src/main/java/com/my/blog/website/CoreApplication.java
+++ src/main/java/com/my/blog/website/CoreApplication.java
@@ -47,49 +47,52 @@ public class CoreApplication {
     /*
      * 把此EmbeddedServletContainerFactory 注入到web容器中
      */
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-        	//拦截所有的/*请求
-            @Override
-            protected void postProcessContext(Context context) {
-                //Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS. 
-                //You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
-
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-        
-    @Bean
-    public Connector httpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-
-        //Set the scheme that will be assigned to requests received through this connector
-        //@param scheme The new scheme
-        connector.setScheme("http");
-
-        //Set the port number on which we listen for requests.
-        // @param port The new port number
-        //release port
-        connector.setPort(8080);
-
-        //Set the secure connection flag that will be assigned to requests received through this connector.
-        //@param secure The new secure connection flag
-        //if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
-        connector.setSecure(false);
-
-        //redirectPort The redirect port number (non-SSL to SSL)
-        connector.setRedirectPort(8443);
-        return connector;
-    }
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//        	//拦截所有的/*请求
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                //Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS. 
+//                //You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
+//
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//        
+//    @Bean
+//    public Connector httpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//
+//        //Set the scheme that will be assigned to requests received through this connector
+//        //@param scheme The new scheme
+//        connector.setScheme("http");
+//
+//        //Set the port number on which we listen for requests.
+//        // @param port The new port number
+//        //release port
+//        connector.setPort(8080);
+//        
+//        connector.setAsyncTimeout(20000);
+//        
+//        
+//        //Set the secure connection flag that will be assigned to requests received through this connector.
+//        //@param secure The new secure connection flag
+//        //if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
+//        connector.setSecure(false);
+//
+//        //redirectPort The redirect port number (non-SSL to SSL)
+//        connector.setRedirectPort(8443);
+//        return connector;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
