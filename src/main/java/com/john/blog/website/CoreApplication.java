e5ecb6bb8702efc3718452f173d85e79342c4593
vincent <vincentzxw0502@163.com>
2018-04-19 14:30:39 +0800

    fix the bug of name of package

--- src/main/java/com/john/blog/website/CoreApplication.java
+++ src/main/java/com/john/blog/website/CoreApplication.java
@@ -1,24 +1,20 @@
 package com.john.blog.website;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import javax.sql.DataSource;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 /*
  * Created by john on 2018/4/17.
