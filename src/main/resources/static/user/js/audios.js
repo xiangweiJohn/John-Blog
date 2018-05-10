1cf22c75b9c86e1977e91ed34952ddd239f6181e
vincent <vincentzxw0502@163.com>
2018-05-10 17:10:17 +0800

    完成写一首/上一首功能

--- src/main/resources/static/user/js/audios.js
+++ src/main/resources/static/user/js/audios.js
@@ -5,10 +5,19 @@ $(document).ready(function() {
 	// 1:获取当前网址
 	var curPath = window.document.location.href;
 	// alert(curPath);
-	// 2：拼接ajax的url需要写的值
-	var url = curPath + "audios";
+	// 2:获得主机地址之后的目录
+	var pathName = window.document.location.pathname;
+	// alert(pathName);
+	var pos = curPath.indexOf(pathName);
+	// alert(pos);
+	// 3:获得主机地址
+	var localhostPath = curPath.substring(0, pos);
+	// alert(localhostPath);
+	// 4：拼接ajax的url需要写的值
+	var url = localhostPath + "/audios";
 	// alert(url);
-	// 3:向后台请求audios集合数据
+
+	// 5:向后台请求audios集合数据
 	$.ajax({
 		async : "false",// async必须设为false，不然获取不到audios的Json集合数据
 		url : url,
@@ -17,9 +26,9 @@ $(document).ready(function() {
 		success : function(data) {
 			// alert(data);
 			// alert(data[0].src);
-			// 4:设audios为全局变量，以便playNext()函数使用
+			// 6:设audios为全局变量，以便playNext()函数使用
 			audios = data;
-			// 5：开始播放
+			// 7：开始播放
 			playStart(audios);
 		},
 		error : function(jqXHR, textStatus, errorThrown) {
@@ -80,24 +89,114 @@ function playNext() {
 }
 
 function isHidden() {
-	// 获取music类的属性hidden
+	// 1:获取music类的属性hidden
 	var isHidden = $(".audio").attr("hidden");
 	// alert(isHidden);
 	if (isHidden != "hidden") {// 如果播放器没隐藏
-		// 将播放器隐藏 动态添加hidden属性
-		// alert("test");
-		// 给audio的div添加hidden属性
+		// 2:给audio的div添加hidden属性
 		$(".audio").attr("hidden", "hidden");
-		// 将背景图的改为白色
+		// 3:给preImg的div添加hidden属性
+		$(".preImg").attr("hidden", "hidden");
+		// 4:给nextImg的div添加hidden属性
+		$(".nextImg").attr("hidden", "hidden");
+		// 5:将背景图的改为白色
 		// alert($(".bg-musc").attr("src"));
-		$(".bg-musc").attr("src", "/user/img/music//hidden.png");
+		$(".bg-musc").attr("src", "/user/img/music/hidden.png");
+		$(".preImg").attr("src", "/user/img/music/hidden.png");
+		$(".nextImg").attr("src", "/user/img/music/hidden.png");
 	} else {// 如果播放器隐藏了
-		// 将播放器隐藏了 动态删除hidden属性
 		// 将audio的div属性hidden删除
 		$(".audio").removeAttr("hidden");
-		// //将背景图的改为播放器背景
-		$(".bg-musc").attr("src", "/user/img/music//bg-musc.png");
+		$(".preImg").removeAttr("hidden");
+		$(".nextImg").removeAttr("hidden");
+		// 将背景图的改为播放器背景
+		$(".bg-musc").attr("src", "/user/img/music/bg-musc.png");
+		$(".preImg").attr("src", "/user/img/music/previous.png");
+		$(".nextImg").attr("src", "/user/img/music/next.png");
 	}
 
 }
+
+function previous() {
+	// alert("pre");
+	// 1:获得audio元素对象 通过jquery获取的某些元素对象无法实现元素对象的某些方法 比如audio
+	var audio = $(".audio");
+	// alert(audio.attr("autoplay"));
+	// 2:获得audio的id属性值，即为当前歌曲在audios集合中所属对象的index
+	var indexCur = audio.attr("id");
+	// alert(indexCur);
+	// 3:播放上一首歌曲
+	if (indexCur != 0) {// 当前歌曲不是第一首歌
+		// alert("test")
+		var indexPre = parseInt(indexCur) - 1;
+		// alert(indexPre);
+		// 1:获得上一首歌的src
+		var srcPre = audios[indexPre].src;
+		// alert(srcPre);
+		// 2:暂停当前歌曲播放
+		var audio = document.getElementsByClassName("audio")[0];
+		audio.pause();
+		// 3:将当前歌曲id改为上一首的
+		var idPre = indexPre;
+		audio.setAttribute("id", idPre);
+		// 4:将当前src为上一首歌的src
+		audio.setAttribute("src", srcPre);
+		// 5:启动播放
+		audio.play();
+	} else {// 当前歌曲是第一首歌
+		// alert("test");
+		// 当前歌曲重新开始播放
+		// 1：暂停当前歌曲播放
+		// audio的某些方法与jQuery存在不兼容的现象。表现为利用jQuery的获取的Media类型元素无法执行这些方法，
+		// 它们必须在用js原生的元素选择器方法(如document.getElementsByClassName())获取元素执行
+		var audio = document.getElementsByClassName("audio")[0];
+		// 2:将当前时间设为0
+		audio.currentTime = 0;
+		// 3:重新播放
+		audio.play();
+	}
+
+}
+
+function next() {
+	// 1:获得audio元素对象 通过jquery获取的某些元素对象无法实现元素对象的某些方法 比如audio
+	var audio = $(".audio");
+	// alert(audio.attr("autoplay"));
+	// 2:获得audio的id属性值，即为当前歌曲在audios集合中所属对象的index
+	var indexCur = audio.attr("id");
+	// alert(indexCur);
+	// alert(audioslength);
+	// 3:播放下一首歌曲
+	if (indexCur < audioslength - 1) {// 当前歌曲不是最后首歌
+		// alert("test")
+		var indexNext = parseInt(indexCur) + 1;
+		// alert(indexNext);
+		// 1:获得下一首歌的src
+		var srcNext = audios[indexNext].src;
+		// alert(srcNext);
+		// 2:暂停当前歌曲播放
+		var audio = document.getElementsByClassName("audio")[0];
+		audio.pause();
+		// 3:将当前歌曲id改为下一首的
+		var idNext = indexNext;
+		audio.setAttribute("id", idNext);
+		// 4：将当前src为下一首歌的src
+		audio.setAttribute("src", srcNext);
+		// 5：启动播放
+		audio.play();
+	} else {// 当前歌曲是最后一首歌
+		// alert("test");
+		// 当前歌曲重新开始播放
+		// 1：暂停当前歌曲播放
+		// audio的某些方法与jQuery存在不兼容的现象。表现为利用jQuery的获取的Media类型元素无法执行这些方法，
+		// 它们必须在用js原生的元素选择器方法(如document.getElementsByClassName())获取元素执行
+		var audio = document.getElementsByClassName("audio")[0];
+		// 2:将当前时间设为0
+		audio.currentTime = 0;
+		// 3:重新播放
+		audio.play();
+	}
+
+}
+
 /* ]]> */
