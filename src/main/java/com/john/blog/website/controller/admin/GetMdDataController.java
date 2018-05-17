7585da4a3e2381e7e430eef88c56123b6184ccc2
vincent <vincentzxw0502@163.com>
2018-05-17 10:18:01 +0800

    在文章发布页添加了一个markdown编辑器

---
+++ src/main/java/com/john/blog/website/controller/admin/GetMdDataController.java
@@ -0,0 +1,21 @@
+package com.john.blog.website.controller.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.john.blog.website.controller.BaseController;
+
+/**
+ * Created by john on 2018/4/17.
+ */
+@Controller()
+@RequestMapping("admin/article/publish/getMdData")
+public class GetMdDataController extends BaseController{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(PageController.class);
+	
+	
+
+}
